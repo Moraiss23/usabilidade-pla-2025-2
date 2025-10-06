c3cdab4f6f854dcdca719ec4e3941f2df673cfe4
Gabriel Moreno Magalhães <823117822@ulife.com.br>
2025-10-06 11:37:42 -0300

    React js eba

--- react-app/meu-primeiro-react/src/index.js
+++ react-app/meu-primeiro-react/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createRoot} from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const domNode = document.getElementById('root')
+const root = createRoot(domNode)
+
+
+const App = () => {
+  return <div style = {{margin: "auto", width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
+    <label for="nome" style={{ display: "block", marginBottom: 4}}>Informe seu nome:</label>
+    <input type="text" id="nome" style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: '100%', borderRadius: 8, outline: 'none', boxSizing: 'border-box'}}/>
+    <button style={{marginTop: 12, paddingTop:8, paddingBottom: 8, backgroundColor: "blueviolet", color: "white", border: "none", width: '100%', borderRadius: 8}}>Enviar</button>
+  </div>
+};
+
+root.render(<App/>)
